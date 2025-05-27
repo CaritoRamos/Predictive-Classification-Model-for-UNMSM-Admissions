992d02aea1ba6773b6791303e2b0c4e4ff6f0f08
Carito Ramos <ana.ramos.neira@outlook.com>
2025-05-27 15:33:48 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -45,5 +45,5 @@ def predecir():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 10000))  # usar el puerto de Render si está disponible
+    port = int(os.environ.get('PORT', 5000)) 
     app.run(host='0.0.0.0', port=port)
